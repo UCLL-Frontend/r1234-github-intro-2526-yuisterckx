ccb0b9d359d658ab4a1b309fdb0d40839a024f63
yuisterckx <yorben.sterckx@student.ucll.be>
2025-11-13 14:38:45 +0100

    demo js 1

---
+++ JS/jsbasis.js
@@ -0,0 +1,25 @@
+let x =10;
+console.log(x);
+
+let kwadraat = x*x;
+console.log(kwadraat);
+const tekst ="hallo";
+
+const waarheid = true;
+let y =11;
+console.log(x==y);
+console.log(x>y);
+
+let tien="10";
+console.log(x === tien);
+
+console.log("apple" < "Apple");
+
+if(x%2 ===0) console.log("even")
+    else{console.log("oneven"); }
+
+let result = x%2===0? "even" : "oneven";
+
+let a =3, b=4;
+console.log(a===3||b===0)
+console.log(a<10&& b > 0);
